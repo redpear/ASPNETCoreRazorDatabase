3b6485432f34d7e335481fcfafbe0a34bad30c99
Angie <redpear@mac.com>
2023-09-29 15:55:49 -0700

    Add project files.

---
+++ ASPNETCoreRazorDatabase/dbo/Tables/clients.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[clients] (
+    [Id]      INT           IDENTITY (1, 1) NOT NULL,
+    [name]    VARCHAR (50)  NOT NULL,
+    [email]   VARCHAR (150) NOT NULL,
+    [phone]   VARCHAR (50)  NULL,
+    [address] VARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
